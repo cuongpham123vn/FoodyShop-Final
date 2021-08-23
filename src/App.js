9b317cf62727e2bdaaf8f56de2aa78315f29d10a
cuong <cuongpn190820000@gmail.com>
2021-08-23 22:29:06 +0700

    all-done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-//feature 1//
 import React from 'react';
-import data from "./data.json";
-import Products from "./components/Products";
+import store from "./store";
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import AdminScreen from './screens/AdminScreen';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: data.products,
-      category:"",
-      sort:"", 
-    };
-  }
   render() {
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/">Foody Shop</a>
-        </header>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              <Link to="/">Foody Shop</Link>
+            </header>
 
-        <main>
-          <div className="content">
-            <div className="main">
-              <Products products={this.state.products}></Products>
-            </div>
-            <div className="sidebar">
-              Cart Items
-            </div>
+            <main>
+              <Route path="/admin" component={AdminScreen}/>
+              <Route path="/" component={HomeScreen} exact/>
+           
+            </main>
+
+            <footer>
+              All Rights Reserved.© Foody Shop 2021
+            </footer>
+
           </div>
-        </main>
-
-        <footer>
-          All Rights Reserved.© Foody Shop 2021
-        </footer>
-
-      </div>
+        </BrowserRouter>
+      </Provider>
     );
-  }
+  };
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,35 +1,106 @@
 import React, { Component } from 'react';
 import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal';
+import Zoom from 'react-reveal/Zoom';
+import { connect } from 'react-redux';
+import { fetchProducts } from '../actions/productActions';
+import { addToCart } from '../actions/cartActions';
 
-export default class Products extends Component {
+ class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: null,
+        };
+    };
+    componentDidMount() {
+        this.props.fetchProducts();
+    };
+    openModel = (product) => {
+        this.setState({ product });
+    };
+    closeModel = () => {
+        this.setState({ product: null });
+    };
     render() {
+        const { product} = this.state;
         return (
             <div>
-                <ul className="products">
-                    {this.props.products.map(product => (
-                        <li key={product._id}>
-                            <div className="product">
-                                <a href={"#" + product._id}>
-                                    <img src={product.image} alt={product.title}></img>
-                                    <p>
-                                        {product.title}
-                                    </p>
-                                </a>
-                                <div className="product-price">
-                                    <div>
-                                        {formatCurrency(product.price)}
-                                    </div>
-                                    <button className="button-primary">
-                                        Add to Cart
-                                    </button>
+                <Fade bottom cascade>
+                    {!this.props.products ? ( 
+                    <div>Loading...</div> 
+                            ) : (
+                        <ul className="products">
+                        {this.props.products.map(product => (
+                            <li key={product._id}>
+                                <div className="product">
+                                    <a href={"#" + product._id} onClick={() => this.openModel(product)}>
+                                        <img src={product.image} alt={product.title}></img>
+                                        <p>
+                                            {product.title}
+                                        </p>
+                                    </a>
+                                    <div className="product-price">
+                                        <div>
+                                            {formatCurrency(product.price)}
+                                        </div>
+                                        <button className="button" onClick={() => this.props.addToCart(product)}>
+                                            Add To Cart
+                                        </button>
 
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                            )
+                    }
+             
+                </Fade>
+                {
+                    product && (
+                        <Modal isOpen={true} onRequestClose={this.closeModel}>
+                            <Zoom>
+                                <button className="close-modal"onClick={this.closeModel}>x</button>
+                                <div className="product-details">
+                                    <img src={product.image} alt={product.title} />
+                                    <div className="product-details-description">
+                                        <p>
+                                            <strong>{product.title}</strong>
+                                        </p>
+                                        <p>
+                                            {product.description}
+                                        </p>
+                                        <p>
+                                            Category Food:{" "}
+                                            {product.categoryFood.map((x)=>(
+                                                <span>{" "} <button className="button">{x}</button></span>
+                                            ))}
+                                        </p>
+                                        <div className="product-price">
+                                            <div>
+                                                {formatCurrency(product.price)}
+                                            </div>
+                                            <button className="button primary" onClick={()=>{
+                                                this.props.addToCart(product);
+                                                this.closeModel();
+                                            }}>
+                                                Add to Cart
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </Zoom>
+                        </Modal>
+                    )
+                }
             </div>
         )
     }
 }
-
+export default connect((state) => ({ products: state.products.filteredItems }), {
+    fetchProducts,
+    addToCart,
+})(Products);
