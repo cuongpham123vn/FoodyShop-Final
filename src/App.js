3adf1283574f481dbf6a349f01f0b0b2ad789a97
Cường <63229833+cuongpham123vn@users.noreply.github.com>
2021-08-29 02:29:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import AdminScreen from './screens/AdminScreen';
+import AboutScreen from './screens/AboutScreen';
+import ContactScreen from './screens/ContactScreen';
 
 class App extends React.Component {
   render() {
@@ -13,10 +15,14 @@ class App extends React.Component {
           <div className="grid-container">
             <header>
               <Link to="/">Foody Shop</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
               <Link to="/admin">Admin</Link>
             </header>
 
             <main>
+              <Route path="/contact" component={ContactScreen} />
+              <Route path="/about" component={AboutScreen} />
               <Route path="/admin" component={AdminScreen}/>
               <Route path="/" component={HomeScreen} exact/>
            
